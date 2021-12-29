0523ed3f7dbfbd7606a33721f1702b97c0473035
Lucas Valenzuela <lucasvalenzuela@users.noreply.github.com>
2021-12-29 18:11:39 +0100

    Condensed filter docs

--- src/miscoperations.jl
+++ src/miscoperations.jl
@@ -17,7 +17,7 @@ end
 """
     Base.sort!(pc::AbstractParticleCollection, prop::Symbol; kwargs...)
 
-Sort the particles in the collection in-place by calling [`Base.sort!`] on each of the [`Particles`] objects.
+Sort the particles in the collection in-place by calling `Base.sort!` on each of the [`Particles`](@ref) objects.
 """
 function Base.sort!(pc::AbstractParticleCollection, prop::Symbol; kwargs...)
     for ptype in keys(pc)
@@ -48,7 +48,7 @@ end
 """
     Base.sort(pc::AbstractParticleCollection, prop::Symbol; [affect,] kwargs...)
 
-Create new particle collection with the particles in the collection sorted by calling [`Base.sort`] on each of the [`Particles`] objects.
+Create new particle collection with the particles in the collection sorted by calling `Base.sort` on each of the [`Particles`](@ref) objects.
 
 If the keyword argument `affect` is a non-empty tuple of `Symbol`s, only the affected properties are kept for the particles.
 The specified affected properties do not have to be available for all particles.
@@ -70,10 +70,11 @@ end
 
 """
     Base.filter!(f, p::AbstractParticles)
+    Base.filter!(f, pc::AbstractParticleCollection)
 
-Filter the particles in-place by a mask returned by the function `f`.
+Filter the particles or collection in-place by masks returned by the function `f`.
 
-The function `f` takes the particles as argument and has to either return a `BitArray` mask with a length of the
+The function `f` takes a particles object as argument and has to either return a `BitArray` mask with a length of the
 number of particles or an array of indices.
 """
 function Base.filter!(f, p::AbstractParticles)
@@ -81,14 +82,6 @@ function Base.filter!(f, p::AbstractParticles)
     return applyind!(p, ind)
 end
 
-"""
-    Base.filter!(f, pc::AbstractParticleCollection)
-
-Filter the particle collection in-place by masks returned by the function `f`.
-
-The function `f` takes the particles as argument and has to either return a `BitArray` mask with a length of the
-number of particles or an array of indices.
-"""
 function Base.filter!(f, pc::AbstractParticleCollection)
     for ptype in keys(pc)
         filter!(f, pc[ptype])
@@ -99,8 +92,9 @@ end
 
 """
     Base.filter(f, p::AbstractParticles; affect=keys(p))
+    Base.filter(f, pc::AbstractParticleCollection[; affect])
 
-Create new particles with them filtered by a mask returned by the function `f`.
+Create new particles or collection with the particles filtered by a mask returned by the function `f`.
 
 The function `f` takes the particles as argument and has to either return a `BitArray` mask with a length of the
 number of particles or an array of indices.
@@ -112,16 +106,6 @@ function Base.filter(f, p::AbstractParticles; affect=keys(p))
     return applyind(p, ind; affect)
 end
 
-"""
-    Base.filter(f, pc::AbstractParticleCollection[; affect])
-
-Create new particle collection with the particles filtered by masks returned by the function `f`.
-
-The function `f` takes the particles as argument and has to either return a `BitArray` mask with a length of the
-number of particles or an array of indices.
-If the keyword argument `affect` is a non-empty tuple of `Symbol`s, only those properties are filtered and added
-to the newly created particles objects.
-"""
 function Base.filter(f, pc::AbstractParticleCollection; affect=nothing)
     pc = copy(pc)
 
@@ -139,19 +123,15 @@ end
 
 """
     Base.filter!(p::AbstractParticles, ids)
+    Base.filter!(pc::AbstractParticleCollection; ids)
 
-Filter the particles in-place by keeping only those with the given IDs.
+Filter the particles or collection in-place by keeping only the particles with the given IDs.
 """
 function Base.filter!(p::AbstractParticles; ids)
     ind = findall_in(p.id, ids)
     return applyind!(p, ind)
 end
 
-"""
-    Base.filter!(pc::AbstractParticleCollection; ids)
-
-Filter the particle collection in-place by keeping only the particles with the given IDs.
-"""
 function Base.filter!(pc::AbstractParticleCollection; ids)
     for ptype in keys(pc)
         filter!(pc[ptype]; ids)
@@ -162,8 +142,9 @@ end
 
 """
     Base.filter(p::AbstractParticles, ids; affect=keys(p))
+    Base.filter(pc::AbstractParticleCollection; ids[, affect])
 
-Create new particles with them filtered by keeping only those with the given IDs.
+Create new particles or collection with them filtered by keeping only the particles with the given IDs.
 
 If the keyword argument `affect` is a non-empty tuple of `Symbol`s, only those properties are filtered and added
 to the newly created particles object.
@@ -173,14 +154,6 @@ function Base.filter(p::AbstractParticles; ids, affect=keys(p))
     return applyind(p, ind; affect)
 end
 
-"""
-    Base.filter(pc::AbstractParticleCollection; ids[, affect])
-
-Create new particle collection with the particles filtered by keeping only those with the given IDs.
-
-If the keyword argument `affect` is a non-empty tuple of `Symbol`s, only those properties are filtered and added
-to the newly created particles objects.
-"""
 function Base.filter(pc::AbstractParticleCollection; ids, affect=nothing)
     pc = copy(pc)
 
@@ -198,8 +171,9 @@ end
 
 """
     Base.filter(p::AbstractParticles, geo::AbstractCosmoGeometry, prop::Symbol=:pos; affect=keys(p))
+    Base.filter(pc::AbstractParticleCollection, geo::AbstractCosmoGeometry, prop::Symbol=:pos[; affect])
 
-Create new particles with them filtered by keeping only those inside the given [`AbstractCosmoGeometry`](@ref).
+Create new particles or collection with them filtered by keeping only the particles inside the given [`AbstractCosmoGeometry`](@ref).
 
 The filter is applied to the property specified.
 If the keyword argument `affect` is a non-empty tuple of `Symbol`s, only those properties are filtered and added
@@ -210,14 +184,6 @@ function Base.filter(p::AbstractParticles, geo::AbstractCosmoGeometry, prop::Sym
     return applyind(p, ind; affect)
 end
 
-"""
-    Base.filter(pc::AbstractParticleCollection, geo::AbstractCosmoGeometry, prop::Symbol=:pos[; affect])
-
-Create new particle collection with the particles filtered by keeping only those with the given IDs.
-
-If the keyword argument `affect` is a non-empty tuple of `Symbol`s, only those properties are filtered and added
-to the newly created particles objects.
-"""
 function Base.filter(
     pc::AbstractParticleCollection,
     geo::AbstractCosmoGeometry,
@@ -236,20 +202,12 @@ function Base.filter(
 
     return pc
 end
-function Base.filter(pc::AbstractParticleCollection, geo::AbstractCosmoGeometry, prop::Symbol=:pos)
-    pc = copy(pc)
-
-    for ptype in keys(pc)
-        pc[ptype] = filter(pc[ptype], geo, prop)
-    end
-
-    return pc
-end
 
 """
     Base.filter!(p::AbstractParticles, geo::AbstractCosmoGeometry, prop::Symbol=:pos)
+    Base.filter!(pc::AbstractParticleCollection, geo::AbstractCosmoGeometry, prop::Symbol=:pos)
 
-Filter the particles in-place by keeping only those inside the given [`AbstractCosmoGeometry`](@ref).
+Filter the particles or collection in-place by keeping only the particles inside the given [`AbstractCosmoGeometry`](@ref).
 
 The filter is applied to the property specified.
 """
@@ -258,14 +216,6 @@ function Base.filter!(p::AbstractParticles, geo::AbstractCosmoGeometry, prop::Sy
     return applyind!(p, ind)
 end
 
-"""
-    Base.filter!(pc::AbstractParticleCollection, geo::AbstractCosmoGeometry, prop::Symbol=:pos)
-
-Filter the particle collection in-place by masks returned by the function `f`.
-
-The function `f` takes the particles as argument and has to either return a `BitArray` mask with a length of the
-number of particles or an array of indices.
-"""
 function Base.filter!(pc::AbstractParticleCollection, geo::AbstractCosmoGeometry, prop::Symbol=:pos)
     for ptype in keys(pc)
         filter!(pc[ptype], geo, prop)
